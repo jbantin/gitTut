5f66e1eb0d6fa8587e7bc7177dc118e2b3fc9ff0
Jörg <jbantin@gmx.de>
2024-12-16 11:44:27 +0100

    ver1

--- index.js
+++ index.js
@@ -6,20 +6,16 @@ const API_KEY = process.env.API_KEY;
 
 console.log(URL);
 
-const fetchWeather = async () => {
+app.use("*", async (req, res) => {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=53.552343789837124&lon=10.017986297607424&appid=${API_KEY}&units=metric`
     );
     const result = await response.json();
-    console.log(result);
+    res.send(result);
   } catch (error) {
     console.log("error", error.message);
   }
-};
-fetchWeather();
-app.use("*", (req, res) => {
-  res.send("oioioi");
 });
 
 app.listen(4000, () => {
